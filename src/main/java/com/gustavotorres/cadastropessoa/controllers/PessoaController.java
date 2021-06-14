3cb20024f643aaeb5e1939f5f0c4376033cc07a6
gustavotrr <gustavotrr@gmail.com>
2021-06-14 17:24:02 -0300

    Criando paginação para o endpoint  "findAll"

--- src/main/java/com/gustavotorres/cadastropessoa/controllers/PessoaController.java
+++ src/main/java/com/gustavotorres/cadastropessoa/controllers/PessoaController.java
@@ -7,6 +7,14 @@ import com.gustavotorres.cadastropessoa.dtos.PessoaDTO;
 import com.gustavotorres.cadastropessoa.services.PessoaService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.PagedModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +22,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -23,11 +32,24 @@ public class PessoaController {
     @Autowired
     private PessoaService pessoaService;
 
-    @GetMapping(produces = {"application/json","application/xml","application/x-yaml"})
-    public ResponseEntity<?> findAll(
-        ) {
+    @Autowired
+    private PagedResourcesAssembler<PessoaDTO> assembler;
 
-            return new ResponseEntity<>(pessoaService.findAll(),HttpStatus.OK);
+    @GetMapping(produces = {"application/json","application/xml","application/x-yaml"})
+        public ResponseEntity<?> findAll(@RequestParam(value = "page", defaultValue = "0") int page,    
+        @RequestParam(value = "size", defaultValue = "12") int size,    
+        @RequestParam(value = "sort", defaultValue = "asc") String sort
+    ) {
+
+            var sortDirection = "desc".equalsIgnoreCase(sort) ? Direction.DESC : Direction.ASC;
+
+            Pageable pageable = PageRequest.of(page, size, Sort.by(sortDirection,"nome"));
+
+            Page<PessoaDTO> pessoas = pessoaService.findAll(pageable);
+            
+            PagedModel<EntityModel<PessoaDTO>> pagedModel = assembler.toModel(pessoas);
+
+            return new ResponseEntity<>(pagedModel,HttpStatus.OK);
 
     }
 
